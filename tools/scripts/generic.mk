419b904479d549e9b8dbe7ce92c9cc9daec2816f
Mihail Chindris <mihail.chindris@analog.com>
2020-07-23 18:56:47 +0300

    tools:scripsts: Add new build variables SRC_DIRS and IGNORE_FILES
    Implement functionality and add description for new variables.
    SRC_DIRS will be used to specify a full folder of sources and
    IGNORE_FILES to remove some files from the build
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- tools/scripts/generic.mk
+++ tools/scripts/generic.mk
@@ -1,3 +1,25 @@
+#------------------------------------------------------------------------------
+#                              UTIL FUNCTIONS
+#------------------------------------------------------------------------------
+# recursive wildcard
+rwildcard = $(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+
+#------------------------------------------------------------------------------
+#                          COMMON INITIALIZATION
+#------------------------------------------------------------------------------
+
+# Get all .c and .h files from SRC_DIRS
+SRCS     += $(foreach dir, $(SRC_DIRS), $(call rwildcard, $(dir),*.c))
+INCS     += $(foreach dir, $(SRC_DIRS), $(call rwildcard, $(dir),*.h))
+
+# Remove ignored files
+SRCS     := $(filter-out $(IGNORE_FILES),$(SRCS))
+INCS     := $(filter-out $(IGNORE_FILES),$(INCS))
+
+#------------------------------------------------------------------------------
+#                          INCLUDE SPECIFIC MAKEFILES
+#------------------------------------------------------------------------------
+
 ifeq (aducm3029,$(strip $(PLATFORM)))
 #Aducm3029 makefile
 include ../../tools/scripts/aducm.mk
