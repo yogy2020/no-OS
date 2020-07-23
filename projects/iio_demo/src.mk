100d0863ce19fb2c148db65f2e4ab02a1212c245
Mihail Chindris <mihail.chindris@analog.com>
2020-07-23 19:34:52 +0300

    projects: iio_demo: update iio_demo makefile
    iio_demo will use the new generic makefile
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- projects/iio_demo/src.mk
+++ projects/iio_demo/src.mk
@@ -1,27 +1,15 @@
-################################################################################
-#									       #
-#     Shared variables:							       #
-#	- PROJECT							       #
-#	- DRIVERS							       #
-#	- INCLUDE							       #
-#	- PLATFORM_DRIVERS						       #
-#	- NO-OS								       #
-#									       #
-################################################################################
+#See No-OS/tool/scripts/src_model.mk for variable description
 
-SRCS := $(PROJECT)/src/main.c
+SRC_DIRS += $(PROJECT)/src
+SRC_DIRS += $(NO-OS)/iio/iio_demo
+
+LIBRARIES += iio
 
 SRCS += $(PLATFORM_DRIVERS)/uart.c					\
 	$(PLATFORM_DRIVERS)/irq.c					\
 	$(NO-OS)/util/xml.c						\
 	$(NO-OS)/util/fifo.c						\
 	$(NO-OS)/util/util.c						\
-	$(NO-OS)/iio/iio.c						\
-	$(NO-OS)/iio/iio_app/iio_app.c					\
-	$(NO-OS)/iio/iio_demo/iio_demo.c
-
-INCS += $(PROJECT)/src/app_config.h					\
-	$(PROJECT)/src/parameters.h
 
 INCS += $(INCLUDE)/xml.h						\
 	$(INCLUDE)/fifo.h						\
@@ -30,10 +18,4 @@ INCS += $(INCLUDE)/xml.h						\
 	$(INCLUDE)/util.h						\
 	$(INCLUDE)/error.h						\
 	$(PLATFORM_DRIVERS)/irq_extra.h					\
-	$(PLATFORM_DRIVERS)/uart_extra.h				\
-	$(NO-OS)/iio/iio.h						\
-	$(NO-OS)/iio/iio_types.h					\
-	$(NO-OS)/iio/iio_app/iio_app.h					\
-	$(NO-OS)/iio/iio_demo/iio_demo.h				\
-	$(NO-OS)/libraries/libtinyiiod/compat.h				\
-	$(NO-OS)/libraries/libtinyiiod/tinyiiod.h
+	$(PLATFORM_DRIVERS)/uart_extra.h				
